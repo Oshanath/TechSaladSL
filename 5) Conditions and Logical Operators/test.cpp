81e8dc4ee33fe215f3bb47ab5bd0685fb95f7e60
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2022-07-14 01:34:43 +0530

    conditions and operators

---
+++ 5) Conditions and Logical Operators/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+
+    int a = 5; // assignment operator
+    int b = 10;
+
+    a = a + 10; // arithmetic operator (+ - * / %)
+    std::cout << a << "\n";
+
+    std::cout << b - a << "\n";
+
+    std::cout << a * b << "\n";
+
+    std::cout << a / b << "\n";
+
+    std::cout << 93 % 10 << "\n"; // modulus, remainder
+
+    int g = 5;
+    int h = -g;  // negation is a unary operator
+
+    // arithmetic operators, logical operators, bitwise operators
+    // unary, binary, ternary
+
+}
