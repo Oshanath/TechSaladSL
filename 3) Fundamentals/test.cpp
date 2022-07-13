df3fc1849bfdc268388e914aa58a8d9df901583c
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2022-07-13 13:13:03 +0530

    fundamentals

---
+++ 3) Fundamentals/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world" << "\n"; // escape characters (newline)
+    std::cout << "hello again." << "\n";
+}
+
+//functions
+// entry point
